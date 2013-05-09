64ecaa912e1817d258622c709034b8f2b23afb66
keul <luca@keul.it>
2013-05-09 16:10:13 +0000

    Inizio sviluppo delle content rule e derivati

---
+++ rer/groupware/notify/action/mail.py
@@ -0,0 +1,201 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+from Acquisition import aq_inner, aq_base, aq_parent
+from OFS.SimpleItem import SimpleItem
+from zope.component import adapts
+from zope.component.interfaces import ComponentLookupError
+from zope.interface import Interface, implements
+from zope.formlib import form
+from zope import schema
+
+from plone.app.vocabularies.groups import GroupsSource
+from plone.app.vocabularies.users import UsersSource
+
+from plone.app.contentrules.browser.formhelper import AddForm, EditForm
+from plone.contentrules.rule.interfaces import IRuleElementData, IExecutable
+
+from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone.utils import safe_unicode
+
+from Products.Archetypes.interfaces import IBaseContent
+
+from rer.groupware.notify import messageFactory as _
+from rer.groupware.notify import logger
+from rer.groupware.room.interfaces import IRoomArea
+from rer.groupware.room.interfaces import IGroupRoom
+
+class IMailForGroupwareNotificationAction(Interface):
+    """Definition of the configuration available for a mail action
+    """
+    subject = schema.TextLine(
+        title = _(u"Subject"),
+        description = _(u"Subject of the message"),
+        required = True
+        )
+
+    source = schema.TextLine(
+        title = _(u"Sender email"),
+        description = _(u"The email address that sends the email. If no email is "
+                         "provided here, it will use the portal from address."),
+         required = False
+         )
+
+    message = schema.Text(
+        title = _(u"Mail message"),
+        description = _('help_message',
+                        default=u"Type in here the message that you want to send. Some "
+                                 "defined content can be replaced: ${title} will be replaced by the title "
+                                 "of the target item. ${url} will be replaced by the URL of the item.\n"
+                                 "${room_title} will be replaced by the name of the room.\n"
+                                 "${room_url} will be replaced by the URL of the room.\n"
+                                 "${area_title} will be replaced by the name of the area.\n"
+                                 "${area_url} will be replaced by the URL of the area.\n"                                 
+                                 ),
+        required = True
+        )
+
+
+class MailForGroupwareNotificationAction(SimpleItem):
+    """
+    The implementation of the action defined before
+    """
+    implements(IMailForGroupwareNotificationAction, IRuleElementData)
+
+    subject = u''
+    message = u''
+    source = u''
+
+    element = 'plone.actions.GroupwareMail'
+
+    @property
+    def summary(self):
+        return _('action_summary',
+                 default=u'Groupware notifications')
+
+
+class MailActionExecutor(object):
+    """The executor for this action.
+    """
+    implements(IExecutable)
+    adapts(Interface, IMailForGroupwareNotificationAction, Interface)
+
+    def __init__(self, context, element, event):
+        self.context = context
+        self.element = element
+        self.event = event
+
+    def __call__(self):
+        context = self.context
+        element = self.element
+        event = self.event
+        recipients = []      
+
+        mailhost = getToolByName(aq_inner(self.context), "MailHost")
+        if not mailhost:
+            raise ComponentLookupError, 'You must have a Mailhost utility to execute this action'
+
+        source = element.source
+        urltool = getToolByName(aq_inner(self.context), "portal_url")
+        portal = urltool.getPortalObject()
+        email_charset = portal.getProperty('email_charset')
+        if not source:
+            # no source provided, looking for the site wide "from" email address
+            from_address = portal.getProperty('email_from_address')
+            if not from_address:
+                raise ValueError, ('You must provide a source address for this '
+                                   'action or enter an email in the portal properties')
+            from_name = portal.getProperty('email_from_name')
+            source = "%s <%s>" % (from_name, from_address)
+
+        obj_title = safe_unicode(event.object.Title())
+        section_title = safe_unicode(context.Title())
+        event_url = event.object.absolute_url()
+        
+        # find parent area and room
+        area = self._getParentArea(context)
+        room = self._getParentRoom(area)
+
+        subject = self.element.subject.replace("${url}", event_url)
+        subject = subject.replace("${title}", obj_title)
+        subject = subject.replace("${room_title}", room.Title())
+        subject = subject.replace("${room_url}", room.absolute_url())
+        subject = subject.replace("${area_title}", area.Title())
+        subject = subject.replace("${area_url}", area.absolute_url())
+        
+        message = self.element.message.replace("${url}", event_url)
+        message = message.replace("${title}", obj_title)
+        message = message.replace("${room_title}", room.Title())
+        message = message.replace("${room_url}", room.absolute_url())
+        message = message.replace("${area_title}", area.Title())
+        message = message.replace("${area_url}", area.absolute_url())
+
+        recipients = self._notification_recipients(room, area)
+
+        # now tranform recipients in a iterator, if needed
+        if type(recipients) == str or type(recipients) == unicode:
+            recipients = [str(recipients),]
+
+        for email_recipient in [r for r in recipients if r]:
+            logger.debug('sending to: %s' % email_recipient)
+
+            try: # sending mail in Plone 4
+                mailhost.send(message, mto=email_recipient, mfrom=source,
+                              subject=subject, charset=email_charset)
+            except: #sending mail in Plone 3
+                mailhost.secureSend(message, email_recipient, source,
+                                    subject=subject, subtype='plain',
+                                    charset=email_charset, debug=False,
+                                    From=source)
+
+        return True
+
+    def _getParentArea(self, context):
+        path = None
+        while path != '/':
+            if IRoomArea.providedBy(context):
+                break
+            context = aq_parent(aq_inner(context))
+            path = '/'.join(context.getPhysicalPath())
+        return context
+
+    def _getParentRoom(self, context):
+        path = None
+        while path != '/':
+            if IGroupRoom.providedBy(context):
+                break
+            context = aq_parent(aq_inner(context))
+            path = '/'.join(context.getPhysicalPath())
+        return context
+
+    def _notification_recipients(self, room, area):
+        acl_users = getToolByName(self.context, 'acl_users')
+        group = acl_users.getGroupById(room.getId()+'.'+area.getId()+'.notify')
+        if group:
+            return ['aaa@asdf.com',]
+
+
+class MailForGroupwareNotificationAddForm(AddForm):
+    """
+    An add form for the mail action
+    """
+    form_fields = form.FormFields(IMailForGroupwareNotificationAction)
+    label = _(u"Add Groupware notification action")
+    description = _(u"A mail action for the Groupware suite.")
+    form_name = _(u"Configure element")
+
+    def create(self, data):
+        a = MailForGroupwareNotificationAction()
+        form.applyChanges(a, self.form_fields, data)
+        return a
+
+
+class MailForGroupwareNotificationEditForm(EditForm):
+    """
+    An edit form for the mail action
+    """
+    form_fields = form.FormFields(IMailForGroupwareNotificationAction)
+    label = _(u"Configure Groupware notification action")
+    description = _(u"A mail action for the Groupware suite.")
+    form_name = _(u"Configure element")
--- rer/groupware/notify/events.py
+++ rer/groupware/notify/events.py
@@ -2,11 +2,24 @@
 
 from zope.i18n import translate
 
+from zope.component import getUtility
+from zope.app.container.interfaces import IObjectAddedEvent, IObjectRemovedEvent
+from zope.app.container.interfaces import INameChooser
+
 from Products.CMFCore.utils import getToolByName
+#from Products.Archetypes.interfaces import IObjectEditedEvent
+from zope.lifecycleevent.interfaces import IObjectModifiedEvent
+
+from plone.contentrules.engine.assignments import RuleAssignment
+from plone.contentrules.engine.interfaces import IRuleAssignmentManager
+from plone.contentrules.engine.interfaces import IRuleStorage
+from plone.app.contentrules.rule import Rule, get_assignments
 
 from rer.groupware.room.interfaces import IRoomArea
 from rer.groupware.notify import logger
 from rer.groupware.notify import messageFactory as _
+
+from rer.groupware.notify.action.mail import MailForGroupwareNotificationAction
 
 
 class CreateNotificationGroupsEvent:
@@ -25,11 +38,13 @@ class CreateNotificationGroupsEvent:
         results = catalog(object_provides=IRoomArea.__identifier__)
 
         for area in results:
-            groups_tool.addGroup(id='%s.%s.notify' % (room_id, area.getId),
+            group_id = '%s.%s.notify' % (room_id, area.getId)
+            groups_tool.addGroup(id=group_id,
                                  title=translate(_(u"${room_title} ${area_title} notifications",
                                                    mapping={"room_title" : room_title,
                                                             "area_title" : area.Title,}),
                                                    context=context.REQUEST))
+            logger.info('Created group %s' % group_id)
             sgm_groups.append('%s.%s.notify' % (room_id, area.getId))
         #sgm_groups.append('%s.hosts' %room_id)
         self.addSGMEntries(context, sgm_groups, '%s.coordinators' % room_id)
@@ -60,8 +75,8 @@ class CreateNotificationRulesEvent:
     """ 
     
     def __init__(self, context, event):
-
         room_title = context.Title()
+        catalog = getToolByName(context, 'portal_catalog')
         
         results = catalog(object_provides=IRoomArea.__identifier__)
 
@@ -76,10 +91,19 @@ class CreateNotificationRulesEvent:
                                                 u'You can click on the following link to see it:\n'
                                                 u'${url}'),
                                       context=context.REQUEST)
-            self.createRule(context, rule_title="%s-%s-created" % (context.getId(), area.getId),
-                            rule_event=IObjectAddedEvent,
-                            subject=subject_created,
-                            message=message_created)
+            rule_title = translate(_('notify_title_created',
+                                     default=u'[${room_title}] notify for new document inside ${area_title}',
+                                     mapping={"room_title" : context.Title(), "area_title": area.Title}),
+                                   context=context.REQUEST)
+            rule_description = translate(_('notify_description_created',
+                                           default=u'All users inside the notification group of the area '
+                                                   u'${area_title} inside the room ${room_title} will be '
+                                                   u'mailed when new contents are added',
+                                           mapping={"room_title" : context.Title(), "area_title": area.Title}),
+                                         context=context.REQUEST)
+            self.createRule(context, area, rule_id="%s-%s-created" % (context.getId(), area.getId), rule_title=rule_title,
+                            rule_description=rule_description, rule_event=IObjectAddedEvent,
+                            subject=subject_created, message=message_created)
 
             subject_modified=translate(_('notify_subj_modified',
                                          default=u'[${room_title}] document modified inside area ${area_title}',
@@ -90,10 +114,19 @@ class CreateNotificationRulesEvent:
                                                  u'You can click on the following link to see it:\n'
                                                  u'${url}'),
                                        context=context.REQUEST)
-            self.createRule(context, rule_title="%s-%s-modified" % (context.getId(), area.getId),
-                            rule_event=IObjectXXX,
-                            subject=subject_modified,
-                            message=subject_modified)
+            rule_title = translate(_('notify_title_modified',
+                                     default=u'[${room_title}] notify for document edited inside ${area_title}',
+                                     mapping={"room_title" : context.Title(), "area_title": area.Title}),
+                                   context=context.REQUEST)
+            rule_description = translate(_('notify_description_modified',
+                                           default=u'All users inside the notification group of the area '
+                                                   u'${area_title} inside the room ${room_title} will be '
+                                                   u'mailed when contents are modified',
+                                           mapping={"room_title" : context.Title(), "area_title": area.Title}),
+                                         context=context.REQUEST)
+            self.createRule(context, area, rule_id="%s-%s-modified" % (context.getId(), area.getId), rule_title=rule_title,
+                            rule_description=rule_description,  rule_event=IObjectModifiedEvent,
+                            subject=subject_modified, message=subject_modified)
 
             subject_deleted=translate(_('notify_subj_deleted',
                                         default=u'[${room_title}] document deleted inside area ${area_title}',
@@ -102,37 +135,42 @@ class CreateNotificationRulesEvent:
             message_deleted=translate(_('notify_msg_modified',
                                         default=u'The document "${title}" has been deleted.\n'),
                                       context=context.REQUEST)
-            self.createRule(context, rule_title="%s-%s-deleted" % (context.getId(), area.getId),
-                            rule_event=IObjectRemovedEvent,
-                            message="%s\n\n%s" %(no_reply_txt,message_deleted),
-                            subject=subject_deleted,
-                            message=message_deleted)
+            rule_title = translate(_('notify_title_deleted',
+                                     default=u'[${room_title}] notify for document deleted inside ${area_title}',
+                                     mapping={"room_title" : context.Title(), "area_title": area.Title}),
+                                   context=context.REQUEST)
+            rule_description = translate(_('notify_description_deleted',
+                                           default=u'All users inside the notification group of the area '
+                                                   u'${area_title} inside the room ${room_title} will be '
+                                                   u'mailed when contents are deleted',
+                                           mapping={"room_title" : context.Title(), "area_title": area.Title}),
+                                         context=context.REQUEST)
+            self.createRule(context, area, rule_id="%s-%s-deleted" % (context.getId(), area.getId), rule_title=rule_title,
+                            rule_description=rule_description, rule_event=IObjectRemovedEvent,
+                            subject=subject_deleted, message=message_deleted)
 
-    def createRule(self, context, rule_title, rule_event, message, subject):
+    def createRule(self, context, area, rule_id, rule_title, rule_description, rule_event, message, subject):
         #create the rule
-        rule=Rule()
-        rule.event=rule_event
-        rule.title=rule_title
+        rule = Rule()
+        rule.__name__ = rule_id
+        rule.event = rule_event
+        rule.title = rule_title
+        rule.description = rule_description
         #add the rule to rule's storage
         storage = getUtility(IRuleStorage)
         chooser = INameChooser(storage)
-        storage[chooser.chooseName(None, rule)] = rule
-        #set the condition and add it to the rule
-        condition=PortalTypeCondition()
-        condition.check_types=types_list
-        rule.conditions.append(condition)  
+        storage[rule_id] = rule
         #set the action and add it to the rule
-        action=MailGroupAction()
+        action = MailForGroupwareNotificationAction()
         
-        action.members = []
-        action.source = None
-        action.groups=[group]
+        action.sender = None
         action.subject=subject
         action.message=message
         rule.actions.append(action)
         #assignment
         rule_id=rule.id.replace('++rule++','')
-        assignable = IRuleAssignmentManager(self.context)
+        assignable = IRuleAssignmentManager(area.getObject())
         assignable[rule_id] = RuleAssignment(rule_id)
         assignable[rule_id].bubbles=True
-        get_assignments(storage[rule_id]).insert('/'.join(self.context.getPhysicalPath()))
+        get_assignments(storage[rule_id]).insert(area.getPath())
+        logger.info('Created rule %s, enabled on %s' % (rule_id, area.Title))
